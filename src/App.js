72ff77b766d0328642d59d2a302c5d7971dbbed3
Philip Lambok <philiplambok71@gmail.com>
2019-11-17 12:29:58 +0700

    add list of waifus with simple handler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+class Waifu extends React.Component {
+  render() {
+    return <span>Waifu {this.props.name}</span>;
+  }
+}
+
+class Waifus extends React.Component {
+  constructor(props) {
+    super(props);
+    const waifus = [
+      { id: 1, name: "Emilia" },
+      { id: 2, name: "Rem" },
+      { id: 3, name: "Kokom1" }
+    ];
+    this.state = {
+      waifus
+    };
+    this.removeWaifu = this.removeWaifu.bind(this);
+  }
+
+  removeWaifu(id) {
+    let updatedWaifus = [];
+    this.state.waifus.forEach(waifu => {
+      if (waifu.id !== id) {
+        updatedWaifus.push(waifu);
+      }
+    });
+    this.setState({ waifus: updatedWaifus });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>List of waifus</h4>
+        {this.state.waifus.map(waifu => (
+          <li key={waifu.id}>
+            <Waifu name={waifu.name} />
+            <button onClick={e => this.removeWaifu(waifu.id)}>Remove</button>
+          </li>
+        ))}
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Waifus />
     </div>
   );
 }
